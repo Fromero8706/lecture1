5b792de2f69523ff91f8b18c639c2a96c1d400c3
Fromero8706 <felipe.romero@students.iaac.net>
2021-03-17 15:58:04 +1100

    Updates

--- assignment1/FR_script01.js
+++ assignment1/FR_script01.js
@@ -1,26 +1,26 @@
 
 // LECTURE 1- MOUSE OVER  
 
-//const img = document.getElementById('myImage')
+const img = document.getElementById('myImage')
 
-//img.addEventListener('mouseOver', mouseOver, false)
-//img.addEventListener('mouseOut', mouseOut, false)
+img.addEventListener('mouseOver', mouseOver, false)
+img.addEventListener('mouseOut', mouseOut, false)
 
-    //function mouseOver() {
-        //document.getElementById('caption').style.display = 'block';
-    //}
+    function mouseOver() {
+        document.getElementById('caption').style.display = 'block';
+    }
 
-    //function mouseOut() {
-        //document.getElementById('caption').style.display = 'none';
+    function mouseOut() {
+        document.getElementById('caption').style.display = 'none';
         
-    //}
+    }
 
 // LECTURE 1- CREATING TABLE 
 
-const x_slider = document.getElementById('x')
-const y_slider = document.getElementById('y')
+const x_slider = document.getElementById( 'x' )
+const y_slider = document.getElementById( 'y' )
 
-Function makeRows(){
+function makeRows(){
     let table = document.getElementById( 'table' )
     if ( table ) {
         table.remove()
@@ -39,13 +39,30 @@ Function makeRows(){
 
 
     for ( i = 0; i < columns; i ++) {
-        const row = document.createElement( ' tr' )
+        const row = document.createElement( 'tr' )
         table.appendChild( row )
 
         for ( j = 0; j < columns; j ++ ) {
-
             const cell = document.createElement( 'td' )
-            cell.innerText = i + '|' + j
+             cell.innerText = i + '|' + j
+
+      // uncomment to generate a random background color for cell on mouseover
+            const color = Math.floor( Math.random() * 16777215 ).toString( 16 )
+
+            cell.addEventListener( 'mouseover', function () {
+            cell.style.background = '#' + color
+            }, false )
+
+            cell.addEventListener( 'mouseout', function () {
+            cell.style.background = 'pink'
+            }, false )
+
+            row.appendChild( cell )
         }
     }
 }
+
+makeRows() //initialise table on page load
+
+
+
